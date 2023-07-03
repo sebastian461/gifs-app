3272bdc9cb2f611e7eb069356e7bca9fb4bf1ca1
Sebastian Alava <sebastiano-461@hotmail.com>
2023-07-03 18:01:40 -0500

    Search Box Tag
    Capturing the content of the input via a local reference using an element created with the @ViewChild decorator

--- src/app/gifs/components/search-box/search-box.component.ts
+++ src/app/gifs/components/search-box/search-box.component.ts
@@ -1,8 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'gifs-search-box',
   templateUrl: './search-box.component.html',
   styleUrls: ['./search-box.component.css'],
 })
-export class SearchBoxComponent {}
+export class SearchBoxComponent {
+  @ViewChild('txtTagInput')
+  public tagInput!: ElementRef<HTMLInputElement>;
+
+  searchTag(): void {
+    const newTag = this.tagInput.nativeElement.value;
+    console.log({ newTag });
+  }
+}
