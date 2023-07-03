6beb7044efa8bd96b14f56640560ea809cbe7009
Sebastian Alava <sebastiano-461@hotmail.com>
2023-07-03 18:44:55 -0500

    Gif Service and Sidebar History
    The service was created to centralize the search information, as well as a history was added to the sidebar from the centralized information

--- src/app/gifs/components/search-box/search-box.component.ts
+++ src/app/gifs/components/search-box/search-box.component.ts
@@ -1,4 +1,5 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
+import { GifsService } from '../../services/gifs.service';
 
 @Component({
   selector: 'gifs-search-box',
@@ -9,8 +10,13 @@ export class SearchBoxComponent {
   @ViewChild('txtTagInput')
   public tagInput!: ElementRef<HTMLInputElement>;
 
+  constructor(private gifsService: GifsService) {}
+
   searchTag(): void {
     const newTag = this.tagInput.nativeElement.value;
-    console.log({ newTag });
+
+    this.gifsService.searchTag(newTag);
+
+    this.tagInput.nativeElement.value = '';
   }
 }
