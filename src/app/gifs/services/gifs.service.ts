ac54bb910eb42226c989161d66ca6c5e0a32e43c
Sebastian Alava <sebastiano-461@hotmail.com>
2023-07-03 19:12:41 -0500

    Service logic
    Limit the number of searches that can be contained and remove duplicate items from the search list

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -12,7 +12,19 @@ export class GifsService {
     return [...this._tagsHistory];
   }
 
-  searchTag(tag: string): void {
+  private organizeHistory(tag: string) {
+    tag = tag.toLowerCase();
+
+    if (this._tagsHistory.includes(tag)) {
+      this._tagsHistory = this._tagsHistory.filter((oldTag) => oldTag !== tag);
+    }
+
     this._tagsHistory.unshift(tag);
+    this._tagsHistory = this._tagsHistory.slice(0, 10);
+  }
+
+  searchTag(tag: string): void {
+    if (tag.length === 0) return;
+    this.organizeHistory(tag);
   }
 }
