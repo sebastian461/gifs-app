8a851fa2fb095d766c3a6b05251a875dd2dbd0f8
Sebastian Alava <sebastiano-461@hotmail.com>
2023-07-03 23:46:06 -0500

    Local Storage
    Made the data persistent in local storage

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -12,7 +12,10 @@ export class GifsService {
   private apyKey: string = 'lemHtfIHEq2vPORvzX7HJkaEw3GZvlyw';
   private serviceUrl: string = 'http://api.giphy.com/v1/gifs';
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {
+    this.loadLocalStorage();
+    console.log('Gifs service ready');
+  }
 
   get tagsHistory(): string[] {
     return [...this._tagsHistory];
@@ -27,6 +30,22 @@ export class GifsService {
 
     this._tagsHistory.unshift(tag);
     this._tagsHistory = this._tagsHistory.slice(0, 10);
+
+    this.saveLocalStorage();
+  }
+
+  private saveLocalStorage(): void {
+    localStorage.setItem('history', JSON.stringify(this._tagsHistory));
+  }
+
+  private loadLocalStorage(): void {
+    if (!localStorage.getItem('history')) return;
+
+    this._tagsHistory = JSON.parse(localStorage.getItem('history')!);
+
+    if (this._tagsHistory.length === 0) return;
+
+    this.searchTag(this._tagsHistory[0]);
   }
 
   /* Petición HTTP a través de fetch */
